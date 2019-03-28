119b07fafdd1a5bbf08dfb802145f160ff759e9c
Troy Kirin <troykirin@gmail.com>
2019-03-27 17:12:38 -0700

    doing tokenze stopwords

--- FortuneCookieClassifier.py
+++ FortuneCookieClassifier.py
@@ -2,39 +2,48 @@
 import pandas as pd
 import numpy as np
 import os
-import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+import nltk
+from nltk.tokenize import word_tokenize
 
 # %% import datasets
-cwd = os.getcwd()
-traindata = pd.read_csv(filepath_or_buffer="./fortunecookiedata/traindata.txt",header=None)
-trainlabels = pd.read_csv(filepath_or_buffer='./fortunecookiedata/trainlabels.txt')
+# cwd = os.getcwd()
+traindata = pd.read_csv(filepath_or_buffer="./fortunecookiedata/traindata.txt",header=None,names=['traindata'])
+trainlabels = pd.read_csv(filepath_or_buffer='./fortunecookiedata/trainlabels.txt',names=['trainlabels'])
 
-stopwords = pd.read_csv(filepath_or_buffer="./fortunecookiedata/stoplist.txt",header=None)
+stopwords = pd.read_csv(filepath_or_buffer="./fortunecookiedata/stoplist.txt",header=None,names=['stopwords'])
 
-testdata = pd.read_csv(filepath_or_buffer='./fortunecookiedata/testdata.txt',header=None)
-testlabels = pd.read_csv(filepath_or_buffer='./fortunecookiedata/testlabels.txt',header=None)
+testdata = pd.read_csv(filepath_or_buffer='./fortunecookiedata/testdata.txt',header=None,names=['testdata'])
+testlabels = pd.read_csv(filepath_or_buffer='./fortunecookiedata/testlabels.txt',header=None,names=['testlabels'])
 
 
 '''Create a dictionary of words in the training data. Utilize the stopwords list to remove insignificant words.'''
 # %% pre-processing
-traindata.columns = ['traindata']
+# traindata.columns = ['traindata']
 traindata.head()
+'''Tokenize traindata'''
 x = traindata['traindata'].str.split()
-print(x)
+# print(x)
+a = x.to_dict()
+print(type(a))
+print(a[1])
 
-stopwords.columns = ['stopwords']
+#%%
+# '''Tokenize stop words'''
+s = stopwords['stopwords']
+s = s.to_list()
+print(s)
+#%%
 stopwords.head()
 
-trainlabels.columns = ['trainlabels']
 trainlabels.head()
 
-testdata.columns = ['traindata']
 traindata.head()
 
-testlabels.columns = ['trainlabels']
 testlabels.head()
 #%%
-splitTrain = traindata['traindata'].str.split()
 traindata.head()
 #%%
 df = x
